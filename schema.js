8e15b5da46c3b141320cb7368ac2bdf504c8492a
Bo Eriksson <bo.eriksson@kindredgrougroupp.com>
2018-01-16 13:31:50 +0100

    schema file

---
+++ schema.js
@@ -0,0 +1,32 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { typeDefs } from '@transaction-history/main/schema';
+
+const endpoint = 'http://localhost:8080';
+const toJSON = res => res.json();
+
+const deposit = (root, {id}) => fetch(`${endpoint}/deposits/${id}`).then(toJSON);
+const deposits = () => fetch(`${endpoint}/deposits`).then(toJSON);
+const withdrawals = () => fetch(`${endpoint}/withdrawals`).then(toJSON);
+
+const resolvers = {
+  Query: {
+      deposits,
+      withdrawals
+  },
+  WithdrawalState: {
+      __resolveType(obj /* ,context, info */) {
+          switch (obj.type) {
+              case 'REQUESTED': return 'Requested';
+              case 'PROCESSING': return 'Processing';
+              case 'HOLD': return 'Hold';
+              case 'REVERSED': return 'Reversed';
+              case 'LEFT': return 'Left';
+              default: return null;
+          }
+      }
+  }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
